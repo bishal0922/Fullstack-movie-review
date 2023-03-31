3c12c71b7701a5fe937a2a4df9e048ec049386e9
Bishal Giri <bishal0922@gmail.com>
2023-03-31 14:26:34 -0500

    set up .env

--- src/main/java/dev/bishal/MovieReview/MovieReviewApplication.java
+++ src/main/java/dev/bishal/MovieReview/MovieReviewApplication.java
@@ -2,12 +2,23 @@ package dev.bishal.MovieReview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+//rest api controller let the spring boot know that this is the main class and not just another class
+@RestController
 public class MovieReviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieReviewApplication.class, args);
 	}
 
+	//this annotation let the spring boot know that this is the root of the api (our GET endpoint)
+	// "/" is the root of the api 
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello World";
+	}
 }
